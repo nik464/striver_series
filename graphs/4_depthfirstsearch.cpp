c372a833a8c77af4a226a5a432d4b1bf3b7e15fd
Nikhil Chaudhary <nikhil18chaudhary@gmail.com>
2023-04-28 22:53:42 +0530

    depth first search

--- graphs/4_depthfirstsearch.cpp
+++ graphs/4_depthfirstsearch.cpp
@@ -1,4 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution{
 private:
+
+void dfs( int node, vector<int>adj[], int vis[], vector<int>&l)
+{
+    vis[node]=1;  // i visited the node
+    l.push_back(node);
+
+    for(auto it: adj[node])
+    {
+        if(!vis[it])
+        dfs(it, adj,vis, l);
+    }
+}
+
+
+public:
+vector<int>dfs(int V, vector<int>adj[])
+{
+    int vis[V]={0};
+
+    int start=0;
+
+    vector<int>l;
+
+    dfs( start, adj, vis, l);
+
+    return l;
+}
+
+};
